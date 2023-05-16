f24df09886683920258f2c849f371a8267067634
qtf0x <vmarias@mines.edu>
2023-05-16 03:06:40 -0600

    build script for executable+tests

---
+++ include/copy.h
@@ -0,0 +1,8 @@
+#ifndef RAYLA_COPY_H
+#define RAYLA_COPY_H
+
+#include <stdlib.h>
+
+void *copy(void *destination, const void *source, size_t num);
+
+#endif // RAYLA_COPY_H
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "copy.h"
+
+int main()
+{
+	printf("Hello, world!\nMy name is Rayla, and I'm so excited to start "
+	       "tracing rays with you! <3 <3\n");
+}
