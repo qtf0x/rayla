ada96254c7613ac8b708ff446f42e527cab360c5
qtf0x <vmarias@mines.edu>
2023-05-18 16:16:19 -0600

    fix newline after attribution

--- update_deps.sh
+++ update_deps.sh
@@ -1,4 +1,6 @@
 curl 'https://raw.githubusercontent.com/torvalds/linux/master/.clang-format' > .clang-format
 
-echo '# From https://github.com/torvalds/linux\n' | cat - .clang-format > /tmp/clang-format.tmp
+attrib_string=$'# From https://github.com/torvalds/linux\n'
+
+echo "$attrib_string" | cat - .clang-format > /tmp/clang-format.tmp
 mv /tmp/clang-format.tmp .clang-format
