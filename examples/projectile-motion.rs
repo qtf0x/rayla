a1cd6ade5fdb49c59526b7ab875d4de94c3d342e
qtf0x <qtf0x@pm.me>
2026-06-16 23:54:17 -0600

    added license/copyright notices

--- examples/projectile-motion.rs
+++ examples/projectile-motion.rs
@@ -1,3 +1,21 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/* This file is part of the Rayla ray tracer.
+ * Copyright (C) 2026 Aster Marias <aster@slware.org>
+ *
+ * Rayla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rayla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Rayla.  If not, see <https://www.gnu.org/licenses/>. */
+
 use std::{
     fs::{self, File},
     io::{self, BufWriter},
